7b807cbf51ac83c215da1b33397abc89f0e52b07
masha14013 <masha14013@rambler.ru>
2019-11-28 00:17:31 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,365 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equip="X-UA-Compatible" content="ie=edge">
+		<title>Облагораживание участков в МО</title>
+
+		<link rel="stylesheet" href="css/css.css" type="text/css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+		<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+		<script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js" integrity="sha384-0pzryjIRos8mFBWMzSSZApWtPl/5++eIfzYmTgBBmXYdhvxPc+XcFEk+zJwDgWbP" crossorigin="anonymous"></script>
+		<script src="https://unpkg.com/scrollreveal"></script>
+	</head>
+	<body id="home">
+		<nav class="navbar navbar-expand-lg bg-dark navbar-dark fixed-top">
+			<div class="container-fluid">
+				<a href="#" class="navbar-brand"><img class="" src="/img/center-photo.png" width="50px"></i></a>
+				<h4>8 (965) 431-91-18</h4>
+				<h2 class="d-none d-md-block d-sm-block d-lg-none">Облагораживание участков</h2>
+				<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarCollapse">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<a href="traktor01.php" class="nav-link">Покос травы</a>
+						</li>
+						<li class="nav-item">
+							<a href="traktor02.php" class="nav-link">Покос трактором</a>
+						</li>
+						<li class="nav-item">
+							<a href="traktor03.php" class="nav-link">Вспашка</a>
+						</li>
+						<li class="nav-item">
+							<a href="traktor04.php" class="nav-link">Культивация</a>
+						</li>
+						<li class="nav-item">
+							<a href="traktor05.php" class="nav-link">Газоны</a>
+						</li>
+						<li class="nav-item">
+							<a href="traktor06.php" class="nav-link">Спил деревьев</a>
+						</li>
+						<li class="nav-item">
+							<a href="traktor07.php" class="nav-link">Миниэкскаватор</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+
+		<header id="home-section">
+			<div class="dark-overlay">
+				<div class="home-inner">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-8 d-none d-lg-block">
+								<h1 class="display-4"><strong>Облагораживание участков в Московской области</strong></h1>
+								<div class="d-flex flex-row">
+									<div class="p-4 align-self-start">
+										<i class="fa fa-check"></i>
+									</div>
+									<div class="p-4 align-self-end">
+										Получайте скидку на большие объемы работ
+									</div>
+								</div>
+								<div class="d-flex flex-row">
+									<div class="p-4 align-self-start">
+										<i class="fa fa-check"></i>
+									</div>
+									<div class="p-4 align-self-end">
+										У нас самые низкие цены
+									</div>
+								</div>
+								<div class="d-flex flex-row">
+									<div class="p-4 align-self-start">
+										<i class="fa fa-check"></i>
+									</div>
+									<div class="p-4 align-self-end">
+										Мы отвечаем за качество
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-4">
+								<div class="card text-center card-form">
+									<div class="card-body">
+										</br><h3>Контакты</h3>
+										<p>Мы оказываем услуги озеленения и благоустройства на загородных участках в Московской области</p>
+										</br></br>
+										<h4 class="smaller-text"><strong>8 (965) 431-91-18</strong></h4>
+										</br>
+										<h4>Николай</h4>
+										</br></br>
+										<a href="https://vk.com/trudolyb" class="btn" target="_blank">Мы Вконтакте</a></br>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+
+		<section id="explore-section">
+			<div class="container">
+				<div class="row">
+					<div class="col text-center">
+						<div class="p-5">
+							<h1 class="display-4"><strong>Фотоотчет</strong></h1>
+							<p class="lead">На сайте вы можете ознакомиться с результатами работы наших специалистов. <a href="traktor_gallary.php">Открыть галерею...</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="photo-slider">
+			<div class="container">
+				<div id="carouselControls" class="carousel slide slider-images" data-ride="carousel">
+					<div class="carousel-inner" id="b-radius">
+						<div class="carousel-item active">
+							<img class="d-block w-100" src="img/4.jpg" alt="">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="img/3.jpg" alt="">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="img/1.jpg" alt="">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="img/2.jpg" alt="">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="img/5.jpg" alt="">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="img/6.jpg" alt="">
+						</div>
+						<div class="carousel-item">
+							<img class="d-block w-100" src="img/7.jpg" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="calc-head-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-5 text-center">
+						<div class="p-5">
+							<h1 class="display-4"><strong class="smaller-text-2">Стоимость работ</strong></h1>
+							<p class="lead"><strong>Внимание!</strong> Итоговая цена может меняться в зависимости от объема работ и существующего грунта на участке. Более точный расчет стоимости на Вашем участке Вы можете получить, набрав номер нашего специалиста:</p>
+							<h4><strong>8 (965) 431-91-18</strong></h4>
+							<h4><strong>Николай</strong></h4></br>
+							<button class="" data-toggle="modal" data-target="#contactModal"><i class="fa fa-calculator fa-3x" aria-hidden="true"></i> Посчитать</button>
+						</div>
+					</div>
+					<div class="col-md-7 text-center">
+						<table class="table table-hover table-success">
+							<thead>
+							    <tr>
+							    	<th scope="col">Услуга</th>
+							    	<th scope="col">Цена</th>
+							    </tr>
+							</thead>
+							<tbody>
+							    <tr class="z">
+							    	<td>Обработка почвы / газон</td>
+							    	<td></td>
+							    </tr>
+							    <tr>
+							    	<td>Вспашка земли</td>
+							    	<td>750 руб/сотка</td>
+							    </tr>
+							    <tr>
+							    	<td>Культивация земли</td>
+							    	<td>750 руб/сотка</td>
+							    </tr>
+							    <tr>
+							    	<td>Обработка целины фрезой</td>
+							    	<td>1.000 руб/сотка</td>
+							    </tr>
+							    <tr>
+							    	<td>Обработка почвы под огород (вспашка + культивация)</td>
+							    	<td>1.500 руб/сотка</td>
+							    </tr>
+							    <tr>
+							    	<td>Обработка почвы под газон с укаткой почвы катком</td>
+							    	<td>2.500 руб/сотка</td>
+							    </tr>
+							    <tr>
+							    	<td>Газон под ключ</td>
+							    	<td>от 4.000 руб/сотка</td>
+							    </tr>
+							    <tr class="z">
+							    	<td>Покос травы</td>
+							    	<td></td>
+							    </tr>
+							    <tr>
+							    	<td>Покос травы триммером</td>
+							    	<td>от 400 руб/сотка</td>
+							    </tr>
+							    <tr>
+							    	<td>Покос трактором с измельчением</td>
+							    	<td>500 руб/сотка</td>
+							    </tr>
+							    <tr>
+							    	<td>Покос борщевика</td>
+							    	<td>1.000 руб/сотка</td>
+							    </tr>
+							    <tr>
+							    	<td>Уборка травы</td>
+							    	<td>от 400 руб/сотка</td>
+							    </tr>
+							    <tr class="z">
+							    	<td>Уборка и расчистка</td>
+							    	<td></td>
+							    </tr>
+							    <tr>
+							    	<td>Спил деревьев и корчевание пней</td>
+							    	<td>индивидуально</td>
+							    </tr>
+							    <tr>
+							    	<td>Спил деревьев методом пром. альпинизма</td>
+							    	<td>от 10.000 руб</td>
+							    </tr>
+							    <tr>
+							    	<td>Измельчение веток / деревьев</td>
+							    	<td>от 10.000 руб</td>
+							    </tr>
+							    <tr>
+							    	<td>Вывоз мусора</td>
+							    	<td>от 8.000 руб/контейнер</td>
+							    </tr>
+							    <tr class="z">
+							    	<td>Аренда минитехники</td>
+							    	<td></td>
+							    </tr>
+							    <tr>
+							    	<td>Миниэкскаватор</td>
+							    	<td>от 2.000 руб/час</td>
+							    </tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<div class="modal fade text-dark" id="contactModal">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="contactModalTitle">Калькулятор</h5>
+					</div>
+					<div class="modal-body">
+						<div class="delivery-price">
+							<div class="form-group">
+								<label for="region">Укажите Ваше местоположение</label>
+								<select id="region" class="form-control">
+									<option selected>Стоимость доставки техники</option>
+								</select>
+							</div>
+						</div>
+						<div class="boxes">
+							<div class="box-a-wrapper">
+								<div class="box box-a">
+									<div class="form-group labels-for-if">
+										<div class="buttarr top s1"></div>
+										<div class="buttarr bott s1"></div>
+										<!--<label for="input_field1" class="input_field">Вспашка земли <span class="price">750 руб/сотка</span></label>-->
+										<p>Вспашка земли 750 руб/сотка</p>
+										<input class="form-control if" type="number" autocomplete="off" size="3" name="num" min="0" placeholder="Введите кол-во соток" id="input_field1"></input><hr>
+									</div>
+									<div class="form-group labels-for-if">
+										<div class="buttarr top s2"></div>
+										<div class="buttarr bott s2"></div>
+										<label for="input_field2" class="input_field">Культивация земли <span class="price">750 руб/сотка</span></label>
+										<input class="form-control if" type="number" autocomplete="off" size="3" name="num" min="0" placeholder="Введите кол-во соток" id="input_field2"></input><hr>
+									</div>
+									<div class="form-group labels-for-if">
+										<div class="buttarr top s3"></div>
+										<div class="buttarr bott s3"></div>
+										<label for="input_field3" class="input_field">Обработка целины фрезой<span class="price"> 1000 руб/сотка</span></label>
+										<input class="form-control if" type="number" autocomplete="off" size="3" name="num" min="0" placeholder="Введите кол-во соток" id="input_field3"></input><hr>
+									</div>
+									<div class="form-group labels-for-if">
+										<div class="buttarr top s4"></div>
+										<div class="buttarr bott s4"></div>
+										<label for="input_field4" class="input_field">Обработка почвы под огород <br />(вспашка + культивация) <span class="price">1500 руб/сотка</span></label>
+										<input class="form-control if" type="number" autocomplete="off" size="3" name="num" min="0" placeholder="Введите кол-во соток" id="input_field4"></input><hr>
+									</div>
+									<div class="form-group labels-for-if">
+										<div class="buttarr top s5"></div>
+										<div class="buttarr bott s5"></div>
+										<label for="input_field5" class="input_field">Обработка почвы под газон с укаткой почвы катком<br /><span class="price">2500 руб/сотка</span></label>
+										<input class="form-control if" type="number" autocomplete="off" size="3" name="num" min="0" placeholder="Введите кол-во соток" id="input_field5"></input><hr>
+									</div>
+									<div class="form-group labels-for-if">
+										<div class="buttarr top s6"></div>
+										<div class="buttarr bott s6"></div>
+										<label for="input_field6" class="input_field">Газон под ключ <span class="price">от 4000 руб/сотка</span></label>
+										<input class="form-control if" type="number" autocomplete="off" size="3" name="num" min="0" placeholder="Введите кол-во соток" id="input_field6"></input><hr>
+									</div>
+									<div class="form-group labels-for-if">
+										<div class="buttarr top s7"></div>
+										<div class="buttarr bott s7"></div>
+										<label for="input_field7" class="input_field">Покос травы и бурьяна <span class="price">от 400 руб/сотка</span></label>
+										<input class="form-control if" type="number" autocomplete="off" size="3" name="num" min="0" placeholder="Введите кол-во соток" id="input_field7"></input><hr>
+									</div>
+									<div class="form-group labels-for-if">
+										<div class="buttarr top s8"></div>
+										<div class="buttarr bott s8"></div>
+										<label for="input_field8" class="input_field">Уборка травы <span class="price">от 400 руб/сотка</span></label>
+										<input class="form-control if" type="number" autocomplete="off" size="3" name="num" min="0" placeholder="Введите кол-во соток" id="input_field8"></input><hr>
+									</div>
+									<div class="form-group labels-for-if">
+										<div class="buttarr top s9"></div>
+										<div class="buttarr bott s9"></div>
+										<label for="input_field9" class="input_field">Покос травы трактором <span class="price">от 8000 руб/га</span></label>
+										<input class="form-control if" type="number" autocomplete="off" size="3" name="num" min="0" placeholder="Введите кол-во гектаров" id="input_field9"></input><hr>
+									</div>
+									<div class="form-group labels-for-if">
+										<div class="buttarr top s10"></div>
+										<div class="buttarr bott s10"></div>
+										<label for="input_field10" class="input_field">Вывоз мусора <span class="price">от 8000 руб/контейнер</span></label>
+										<input class="form-control if" type="number" autocomplete="off" size="3" name="num" min="0" placeholder="Введите кол-во контейнеров" id="input_field10"></input><hr>
+									</div>
+									<div class="form-group labels-for-if">
+										<label for="input_field11" class="input_field">Миниэкскаватор <br><span class="price">(Цена высчитывается индивидуально)</span></label>
+										<input disabled class="form-control if" autocomplete="off" size="3" name="num" placeholder="Цена высчитывается индивидуально" id="input_field11"></input><hr>
+									</div>
+									<div class="form-group labels-for-if">
+										<label for="input_field12" class="input_field">Спил деревьев и выкорчевка пней<br> <span class="price">(Цена высчитывается индивидуально)</span></label>
+										<input disabled class="form-control if" autocomplete="off" size="3" name="num" placeholder="Цена высчитывается индивидуально" id="input_field12"></input><hr>
+									</div>
+								</div>
+							</div>
+							<div class="box box-b">
+								<h3 align="center" id="sum"></br>Здесь будет результат...</h3>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<section id="warning" class="text-center p-5">
+			<p>*Данный интернет-сайт носит исключительно информационный характер и ни при каких условиях не является публичной офертой, определяемой положениями Статьи 437 (2) ГК РФ.</p>
+		</section>
+
+		<footer id="footer" class="text-center p-5">
+			<p>2019 &copy; обработка-участка.рф</p>
+		</footer>
+
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+	</body>
+</html>
